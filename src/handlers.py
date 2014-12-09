0e195469dd5f70b650af4449f568100bc706e5fd
Cristobal <cganterh@gmail.com>
2014-12-09 19:00:44 -0300

    Fixed FileGoupsHandler not validating group path.

--- src/handlers.py
+++ src/handlers.py
@@ -2,7 +2,9 @@
 
 import re
 
-from os.path import abspath, join as joinpath
+from os.path import abspath, join as joinpath, \
+                    sep as pathsep, isdir, \
+                    exists as pathexists, isfile
 from tornado.web import StaticFileHandler
 
 class FileGoupsHandler(StaticFileHandler):
@@ -24,3 +26,37 @@ class FileGoupsHandler(StaticFileHandler):
     @classmethod
     def register_group(cls, group_id, group_root):
         cls.groups[group_id] = group_root
+
+    def validate_absolute_path(self, root, absolute_path):
+        root = abspath(root)
+        # os.path.abspath strips a trailing /
+        # it needs to be temporarily added back for requests
+        # to root/
+        abspath_sw = (absolute_path + pathsep).startswith
+        sw_root = abspath_sw(root)
+        sw_any = any(abspath_sw(group_root)
+                     for group_root in self.groups.values())
+        if not sw_root and not sw_any:
+            raise HTTPError(403,
+                '%s is not in root static directory or' \
+                'any group root.', self.path)
+                
+        if (isdir(absolute_path) and
+                self.default_filename is not None):
+            # need to look at the request.path here for when
+            # path is empty but there is some prefix to the
+            # path that was already trimmed by the routing
+            if not self.request.path.endswith("/"):
+                self.redirect(self.request.path + "/",
+                              permanent=True)
+                return
+            absolute_path = joinpath(absolute_path,
+                                     self.default_filename)
+                                     
+        if not pathexists(absolute_path):
+            raise HTTPError(404)
+            
+        if not os.path.isfile(absolute_path):
+            raise HTTPError(403, "%s is not a file", self.path)
+            
+        return absolute_path
