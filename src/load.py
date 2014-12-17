e123e767f74f8d5bbe8be51075bb20c1a74246ca
Cristobal <cganterh@gmail.com>
2014-12-16 21:38:34 -0300

    The loading functions in src.load can be now called with the module name instead of the module object.

--- src/load.py
+++ src/load.py
@@ -1,16 +1,27 @@
+import sys
+
 from importlib import import_module
 from os.path import dirname
 from pkgutil import iter_modules
 from types import ModuleType
 from .boiler_ui_module import BoilerUIModule
 
+
+def get_module(module_name):
+    if isinstance(module_name, str):
+        return sys.modules[module_name]
+    else:
+        return module_name
+
 def load_python_modules(package):
+    package = get_module(package)
     directory = dirname(package.__file__)
     modules = [import_module('.' + name, package.__name__)
                for _, name, _ in iter_modules([directory])]
     return modules
 
 def load_boiler_ui_modules(package, app):
+    package = get_module(package)
     python_modules = load_python_modules(package)
     package.boiler_ui_modules = [member
         for module in python_modules
