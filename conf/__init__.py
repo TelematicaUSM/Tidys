bef631906870fd66d3274ae828db414c50aa5637
Cristobal <cganterh@gmail.com>
2015-01-18 21:20:27 -0300

    Created new module for the command line interface. The property "justify-content" of the header was changed to "space-between".

--- conf/__init__.py
+++ conf/__init__.py
@@ -4,6 +4,6 @@ from . import log
 
 app_name = 'TornadoBoiler'
 author = 'Cristóbal Ganter'
-port = 9888
+port = 52000
 debug = True
 user_scalable_viewport = 'no'   #accepted values are 'yes' and 'no'
--- run.py
+++ run.py
@@ -1,32 +1,12 @@
 # -*- coding: UTF-8 -*-
 
-import readline
-
 from threading import Thread
 from logging import info
 from tornado.ioloop import IOLoop
 from conf import app_name, port
 from controller import app
+from src import cli
 
-commands = {
-    'help': {
-        'desc': 'Shows this help message.',
-        'syns': ('help', 'h', )},
-    'close': {
-        'desc': 'Closes the application.',
-        'syns': ('close', 'q', 'quit', 'exit', 'stop', )},
-}
-
-def print_help():
-    print('\nHelp:')
-    for name, data in commands.items():
-        print(
-            '\t%s: %s\n\t\tSynonyms: %s.\n' % (
-                name,
-                data['desc'],
-                ', '.join(data['syns'])
-            )
-        )
 
 info('%s: Starting on port %d ...', app_name, port)
 app.listen(port)
@@ -35,17 +15,7 @@ ioloop = IOLoop.instance()
 t = Thread(target=lambda:ioloop.start())
 t.start()
 
-print_help()
-
-while True:
-    i = input('$ ')
-    if i in commands['close']['syns']:
-        info('%s: Closing ...', app_name)
-        break
-    elif i in commands['help']['syns']:
-        print_help()
-    else:
-        print("'%s' is not a command." % i)
+cli.start()
 
 ioloop.add_callback(lambda:ioloop.stop())
 t.join()
