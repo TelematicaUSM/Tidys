70c3582989e6b5f53db1a9b2b2dff336db0eb1d7
Cristobal <cganterh@gmail.com>
2015-04-13 20:19:57 -0300

    All tasks in todo.md where completed: -   add an in-place javascript insertion helper -   solve this warning:
            export GEM_HOME=./gems && ./gems/bin/sass --update static/scss:static/css
              directory static/css
            WARNING: The box-sizing mixin is deprecated and will be removed in the next major version release. This property can now be used un-prefixed.
                     on line 410 of static/scss/bourbon_files/_bourbon-deprecated-upcoming.scss, in `box-sizing'
                     from line 5 of static/scss/base.scss
    
                  write static/css/base.css
                  write static/css/base.css.map
                  write static/css/boxes.css
                  write static/css/boxes.css.map
                  write static/css/layout.css
                  write static/css/layout.css.map
    
    -   move this to the configuration:
    
            for module in app.ui_modules.values():
                if issubclass(module, BoilerUIModule):
                    module.add_handler(app)
    
    -   Replace chainmap in layout.html with just `globals()`.
         ~~evaluate if chainmap in layout.html can be replaced
         with dict.update~~
    -   add code_related_message(print_f, code_path) in
        messages.py
    -   add header-title id to the default titles
    -   add proxy_url to conf package using a url method to
        concatenate all parts

--- src/messages.py
+++ src/messages.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 
-from logging import info
-from conf import app_name, port, proxy_scheme, proxy_host, \
-                 proxy_port
+import conf
+from sys import exit
+from logging import info, debug, error, critical
 
 def closing():
     info('%s: Closing ...', app_name)
@@ -15,13 +15,49 @@ def starting():
 
 def wellcome():
     print(
-        'Wellcome to %s! Open %s://%s%s in your browser.' %
-        (app_name, proxy_scheme, proxy_host,
-         ':%d'%proxy_port if proxy_port else '')
+        'Wellcome to {.app_name}! Open {.proxy_url} in '
+        'your browser.'.format(conf)
     )
 
-def file_not_found(print_f, function_name, file_name):
+def code_related_message(code_path, message, print_f=print):
     print_f(
-        '%s: file "%s" could not be found!' %
-        (function_name, file_name)
+        '{}: {}'.format(code_path, message)
+    )
+
+def code_info(code_path, message):
+    code_related_message(code_path, message, print_f=info)
+
+def code_debug(code_path, message):
+    code_related_message(code_path, message, print_f=debug)
+
+def code_error(code_path, message):
+    code_related_message(code_path, message, print_f=error)
+
+def code_critical(code_path, message):
+    code_related_message(code_path, message,
+                         print_f=critical)
+    closing()
+    exit()
+
+def file_not_found(code_path, file_name, print_f=error):
+    code_related_message(code_path,
+        'file "{}" could not be found!'.format(file_name),
+        print_f)
+
+def exhausted_tries(code_path):
+    code_critical(code_path,
+        'We exhausted all tries in an algorithm and '
+        'didn\'t have luck. Try incrementing the maximum'
+        'number of tries.')
+
+def impossible_condition(code_path):
+    code_critical(code_path,
+                  'An impossible condition was met!')
+
+def obj_creation_error(code_path, cls, *args, **kwargs):
+    code_error(
+        code_path,
+        'Unable to create object from class {!r}, '
+        'args={!r} and kwargs={!r}'.format(cls, args,
+                                           kwargs)
     )
