9b2fd3a6bac52a6d6fe4dfcaf287882eef85f14b
Cristobal <cganterh@gmail.com>
2015-04-23 17:18:12 -0300

    Now all BoilerUIModules add their external files via add_ext_file automatically.

--- src/ui_methods.py
+++ src/ui_methods.py
@@ -1,5 +1,16 @@
-def add_ext_file(handler, filename):
+def add_ext_file(handler, filenames, static_url_func=None):
     if not hasattr(handler, 'ext_files'):
         handler.ext_files = []
-    handler.ext_files.append(filename)
+        
+    if not static_url_func:
+        static_url_func = handler.static_url
+    
+    if isinstance(filenames, str):
+        handler.ext_files.append(
+            static_url_func(filenames)
+        )
+    else:
+        handler.ext_files.extend(static_url_func(filename)
+                                 for filename in filenames)
+                                 
     return ''
