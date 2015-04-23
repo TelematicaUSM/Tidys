1c3d8066ecb9150e5fc58140273e95ad3be6d79d
Cristobal <cganterh@gmail.com>
2015-04-22 21:34:26 -0300

    Fixed error in which normalize.js overwrites the margins of h1. Deleted js.html helper in favor of include_ext_file.

--- src/ui_methods.py
+++ src/ui_methods.py
@@ -3,9 +3,3 @@ def add_ext_file(handler, filename):
         handler.ext_files = []
     handler.ext_files.append(filename)
     return ''
-
-def add_module_file(handler, filename):
-    if not hasattr(handler, 'module_files'):
-        handler.module_files = []
-    handler.module_files.append(filename)
-    return ''
