b83da2f6f66d6554cb383b7d79c1f0dcbd6e9c32
Cristobal <cganterh@gmail.com>
2015-11-12 12:46:05 -0300

    Update

--- conf/__init__.py
+++ conf/__init__.py
@@ -20,8 +20,7 @@ debug = True
 port = 52002
 
 proxy_scheme = 'http'
-proxy_host = 'mem.zoro.cganterh.net'
-# proxy_host = 'mem.robin.cganterh.net'
+proxy_host = 'aka.aa.cganterh.net'
 proxy_port = None
 _netloc = proxy_host + (':' + str(proxy_port)
                         if proxy_port else '')
@@ -32,20 +31,14 @@ proxy_url = urlunparse(
 # CLIENT SPECIFIC:
 ws_scheme = 'ws'
 
+ws_reconnect_interval = 20
+"""In seconds. See """
+
 # accepted values are 'yes' and 'no'
 user_scalable_viewport = 'no'
 
 # SERVER SPECIFIC:
 root_path = environ.get('AA_PATH', '')
-_z_secrets_file = 'secrets/secrets.json'
-_z_google_secrets_file = 'secrets/' \
-    'client_secret_157405624098-' \
-    'rfhsi6ovr7lugc0f1u84ntuoufpmsfjr.apps.' \
-    'googleusercontent.com.json'
-_r_secrets_file = 'secrets_robin/secrets.json'
-_r_google_secrets_file = 'secrets_robin/client_secret_'\
-    '574237562896-8fc1unhpaqr5idf7bamouqr7l4ruvik1.apps.' \
-    'googleusercontent.com.json'
 
 secrets_file = path.join(root_path, _z_secrets_file)
 google_secrets_file = path.join(root_path,
