cb66b73f27e995a7f935d4ab027a352db4d6af80
Felipe <pipegreyback@gmail.com>
2015-11-15 17:30:51 -0300

    Update on presentation.

--- panels/presentation_test/__init__.py
+++ panels/presentation_test/__init__.py
@@ -2,10 +2,10 @@ import src
 
 
 class RemoteTestPanel(src.boiler_ui_module.BoilerUIModule):
-    id_ = 'remote-test'
+    id_ = 'presentation_test'
     classes = {'scrolling-panel', 'system-panel'}
-    name = 'Remote Test'
+    name = 'Presentation Test'
 
     def render(self):
         return self.render_string(
-            '../panels/remote_test/remote_test.html')
+            '../panels/presentation_test/presentation_test.html')
